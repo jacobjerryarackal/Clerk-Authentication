fef0ae32058b6bb26ca6a8c83fc7ac556666ffa5
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2024-07-15 09:52:13 +0530

    Files Uploaded

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css'
+import Header from '@/components/header'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
